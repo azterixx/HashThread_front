d0e07e640b1f25175819777c476ce9a7e09469b3
maxim <zerk33215@gmail.com>
2025-02-22 04:51:04 +0500

    fix animation

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -8,19 +8,11 @@ export const ActionButton: React.FC<ActionButtonProps> = ({
 }) => {
   return (
     <div className="relative inline-block overflow-visible">
-      {/*
-        Класс group нужен для того, чтобы дочерние элементы могли
-        реагировать на hover кнопки через group-hover.
-      */}
       <button
         {...props}
         className="group flex h-[32px] min-w-[40px] items-center justify-center gap-x-[4px] overflow-visible rounded-[16px] bg-bgLighter px-[4px] py-[8px]"
       >
-        {/*
-          Внедряем внутрь ещё один контейнер,
-          который анимируем при hover через group-hover.
-        */}
-        <div className="flex items-center justify-center transition-transform duration-300 group-hover:-translate-y-2 group-hover:scale-105">
+        <div className="flex items-center justify-center transition-transform delay-100 duration-200 group-hover:-translate-y-[3px] group-hover:scale-105">
           {children}
         </div>
       </button>
