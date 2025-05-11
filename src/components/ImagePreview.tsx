6a23fe75ce936f41bd830bad62a135c29c8ba1d9
2Revol2 <maksimbaluta02@gmail.com>
2025-05-11 16:55:56 +0300

    feat: add photo

---
+++ src/components/ImagePreview.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImagePreviewProps {
+  images: File[];
+}
+export const ImagePreview = ({ images }: ImagePreviewProps) => {
+  const [previews, setPreviews] = useState<string[]>([]);
+
+  useEffect(() => {
+    const objectUrls = images.map((file) => URL.createObjectURL(file));
+    setPreviews(objectUrls);
+
+    return () => {
+      objectUrls.forEach((url) => URL.revokeObjectURL(url));
+    };
+  }, [images]);
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {previews.map((src, index) => (
+        <div
+          key={index}
+          className="relative h-32 w-32"
+        >
+          <img
+            src={src}
+            alt={`preview-${index}`}
+            className="h-full w-full object-cover"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/shared/api/Thread/api.ts
+++ src/shared/api/Thread/api.ts
@@ -1,13 +1,23 @@
 import { baseInstance } from "../base";
 import { PostThreadResponse } from "../types/types";
 
-export const postThread = async (text: string) =>
-  (
-    await baseInstance.post<PostThreadResponse>(
-      "/thread",
-      { text },
-    )
-  ).data;
+export const postThread = async (text: string, files?: File[]) => {
+  const formData = new FormData();
+  formData.append("text", text); // Добавляем текст в formData
+  files?.forEach((file) => formData.append("files", file)); // Добавляем файлы
+
+  const response = await baseInstance.post<PostThreadResponse>(
+    "/thread",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    },
+  );
+
+  return response.data;
+};
 
 export const toggleLikeThread = async (threadId: string) =>
   (
