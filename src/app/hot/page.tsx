c786e87ee8a52edcd64dec1ee8630a2a49a5ae1d
2Revol2 <maksimbaluta02@gmail.com>
2025-06-13 12:45:45 +0300

    fix: fix hot page

--- src/app/hot/page.tsx
+++ src/app/hot/page.tsx
@@ -3,7 +3,7 @@ import { Feed } from "@/components/Feed";
 export default function LikedThreads() {
   return (
     <div className="mx-auto mt-3 w-full overflow-y-auto rounded-lg bg-bgDark md:w-1/2">
-      <Feed type="likedThreads" sort="hot" />
+      <Feed sort="hot" />
     </div>
   );
 }
