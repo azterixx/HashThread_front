be7b6b75ea49f9c7d3383da325a79682833e7631
2Revol2 <maksimbaluta02@gmail.com>
2025-06-17 10:51:08 +0300

    feat: add boring avatars

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -1,20 +1,41 @@
-import { cn } from "@/shared/lib/utils";
-import { memo } from "react";
+import Avatar from "boring-avatars";
+import { memo, useMemo } from "react";
 
 interface UserIconProps {
   size: "lg" | "md";
 }
 
+function getCookie(name: string): string | undefined {
+  const match = document.cookie.match(new RegExp(`(?:^|; )${name}=([^;]*)`));
+  return match ? decodeURIComponent(match[1]) : undefined;
+}
+
 export const UserIcon = memo(({ size }: UserIconProps) => {
+  const avatarSize = size === "lg" ? 36 : 24;
+
+  const variants = [
+    "beam",
+    "marble",
+    "pixel",
+    "sunset",
+    "ring",
+    "bauhaus",
+  ] as const;
+
+  const userId = getCookie("userId") ?? "guest"; // fallback, если куки нет
+
+  // Чтоб всегда был один и тот же вариант — выбираем по userId
+  const variantIndex = userId
+    .split("")
+    .reduce((sum, char) => sum + char.charCodeAt(0), 0) % variants.length;
+
+  const selectedVariant = variants[variantIndex];
+
   return (
-    <div>
-      <div
-        className={cn(
-          "rounded-full bg-[#999999]",
-          size === "lg" && "h-9 w-9",
-          size === "md" && "h-6 w-6",
-        )}
-      />
-    </div>
+    <Avatar
+      size={avatarSize}
+      name={userId}
+      variant={selectedVariant}
+    />
   );
 });
