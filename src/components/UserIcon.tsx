14de0ab0313cbde56936bd93307630d800a8ab37
2Revol2 <maksimbaluta02@gmail.com>
2025-06-17 10:52:01 +0300

    fix: fix user icon

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -1,41 +1,30 @@
 import Avatar from "boring-avatars";
-import { memo, useMemo } from "react";
+import { memo } from "react";
 
 interface UserIconProps {
   size: "lg" | "md";
 }
 
-function getCookie(name: string): string | undefined {
-  const match = document.cookie.match(new RegExp(`(?:^|; )${name}=([^;]*)`));
-  return match ? decodeURIComponent(match[1]) : undefined;
-}
-
 export const UserIcon = memo(({ size }: UserIconProps) => {
   const avatarSize = size === "lg" ? 36 : 24;
 
-  const variants = [
+  const variant = [
     "beam",
     "marble",
+    "beam",
     "pixel",
     "sunset",
     "ring",
     "bauhaus",
   ] as const;
 
-  const userId = getCookie("userId") ?? "guest"; // fallback, если куки нет
-
-  // Чтоб всегда был один и тот же вариант — выбираем по userId
-  const variantIndex = userId
-    .split("")
-    .reduce((sum, char) => sum + char.charCodeAt(0), 0) % variants.length;
-
-  const selectedVariant = variants[variantIndex];
+  const randomVariant = variant[Math.floor(Math.random() * variant.length)];
 
   return (
     <Avatar
       size={avatarSize}
-      name={userId}
-      variant={selectedVariant}
+      name={Math.random().toString()}
+      variant={randomVariant}
     />
   );
 });
